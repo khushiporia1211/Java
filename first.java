555147525e6df152ff3889fb2847b73c77dfc60b
khushi <khushiporia09@gmail.com>
2025-12-29 13:50:09 +0530

    Initial commit

--- first.java
+++ first.java
@@ -1,6 +1,5 @@
-import java.lang.*;
-class first {
+public class First{
     public static void main(String[] args){
-        System.out.println("hello world");
+        System.out.print("Hello");
     }
 }
